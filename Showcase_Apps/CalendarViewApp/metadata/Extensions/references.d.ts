90833e4186098010ca71aeee5652e908a094bbbc
Robin Kuck <robin.kuck@sap.com>
2021-12-04 22:44:55 +0100

    Fix IntelliSense for MDK 6 / Nativescript 7

--- Showcase_Apps/CalendarViewApp/metadata/Extensions/references.d.ts
+++ Showcase_Apps/CalendarViewApp/metadata/Extensions/references.d.ts
@@ -1,2 +1,1 @@
-/// <reference path="{PATH_TO_MDK_CLIENT}/node_modules/tns-platform-declarations/ios.d.ts" />
-/// <reference path="{PATH_TO_MDK_CLIENT}/node_modules/tns-platform-declarations/android-23.d.ts" />
+/// <reference path="{PATH_TO_MDK_CLIENT}/node_modules/@nativescript/types/index.d.ts" />
