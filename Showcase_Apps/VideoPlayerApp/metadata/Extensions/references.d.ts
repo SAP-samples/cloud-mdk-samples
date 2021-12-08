8fe4b5b0c1bce172abb51c2713ac77dddc8b3a8b
robinkuck <robin.kuck@sap.com>
2021-12-08 08:51:06 +0000

    remove old video player folder

--- Showcase_Apps/VideoPlayerApp/metadata/Extensions/references.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="{PATH_TO_MDK_CLIENT}/node_modules/tns-platform-declarations/ios.d.ts" />
-/// <reference path="{PATH_TO_MDK_CLIENT}/node_modules/tns-platform-declarations/android-23.d.ts" />
