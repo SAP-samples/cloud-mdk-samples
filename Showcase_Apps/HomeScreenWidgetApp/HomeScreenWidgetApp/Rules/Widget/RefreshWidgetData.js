9cfb1f464c8e50ac0d36b148f432cc9f3396a8fe
Robin Kuck <robin.kuck@sap.com>
2024-03-07 14:36:33 +0000

    Pass sales orders count for each lifecycle status

--- Showcase_Apps/HomeScreenWidgetApp/HomeScreenWidgetApp/Rules/Widget/RefreshWidgetData.js
+++ Showcase_Apps/HomeScreenWidgetApp/HomeScreenWidgetApp/Rules/Widget/RefreshWidgetData.js
@@ -3,12 +3,14 @@
  * @param {IClientAPI} clientAPI
  */
 export default async function RefreshWidgetData(clientAPI) {
-    let salesOrdersCount = await clientAPI.count('/HomeScreenWidgetApp/Services/com_sap_edm_sampleservice_v2.service', 'SalesOrderHeaders');
+    let newSalesOrdersCount = await clientAPI.count('/HomeScreenWidgetApp/Services/com_sap_edm_sampleservice_v2.service', 'SalesOrderHeaders', "$filter=LifeCycleStatus eq 'N'");
+    let acceptedSalesOrdersCount = await clientAPI.count('/HomeScreenWidgetApp/Services/com_sap_edm_sampleservice_v2.service', 'SalesOrderHeaders', "$filter=LifeCycleStatus eq 'A'");
+    let rejectedSalesOrdersCount = await clientAPI.count('/HomeScreenWidgetApp/Services/com_sap_edm_sampleservice_v2.service', 'SalesOrderHeaders', "$filter=LifeCycleStatus eq 'R'");
     let platformModule = clientAPI.nativescript.platformModule;
 
     if (platformModule.isIOS) {
         let defaults = NSUserDefaults.alloc().initWithSuiteName('group.' + NSBundle.mainBundle.bundleIdentifier);
-        defaults.setObjectForKey(salesOrdersCount.toString(), 'salesOrdersCount');
+        defaults.setObjectForKey({ 'New': newSalesOrdersCount.toString(), 'Accepted': acceptedSalesOrdersCount.toString(), 'Rejected': rejectedSalesOrdersCount.toString() }, 'salesOrdersCount');
         WidgetHelper.refreshWidgetData();
     }
 }
