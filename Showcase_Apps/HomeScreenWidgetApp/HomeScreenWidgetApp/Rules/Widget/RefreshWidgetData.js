ed4da4548f35ffb264735f07c6de8a05ed88af24
Robin Kuck <robin.kuck@sap.com>
2024-04-21 11:08:26 +0000

    Home Screen Widget App: Update sample service to v4

--- Showcase_Apps/HomeScreenWidgetApp/HomeScreenWidgetApp/Rules/Widget/RefreshWidgetData.js
+++ Showcase_Apps/HomeScreenWidgetApp/HomeScreenWidgetApp/Rules/Widget/RefreshWidgetData.js
@@ -2,9 +2,9 @@
  * @param {IClientAPI} clientAPI
  */
 export default async function RefreshWidgetData(clientAPI) {
-    let newSalesOrdersCount = await clientAPI.count('/HomeScreenWidgetApp/Services/com_sap_edm_sampleservice_v2.service', 'SalesOrderHeaders', "$filter=LifeCycleStatus eq 'N'");
-    let acceptedSalesOrdersCount = await clientAPI.count('/HomeScreenWidgetApp/Services/com_sap_edm_sampleservice_v2.service', 'SalesOrderHeaders', "$filter=LifeCycleStatus eq 'A'");
-    let rejectedSalesOrdersCount = await clientAPI.count('/HomeScreenWidgetApp/Services/com_sap_edm_sampleservice_v2.service', 'SalesOrderHeaders', "$filter=LifeCycleStatus eq 'R'");
+    let newSalesOrdersCount = await clientAPI.count('/HomeScreenWidgetApp/Services/com_sap_edm_sampleservice_v4.service', 'SalesOrderHeaders', "$filter=LifeCycleStatus eq 'N'");
+    let acceptedSalesOrdersCount = await clientAPI.count('/HomeScreenWidgetApp/Services/com_sap_edm_sampleservice_v4.service', 'SalesOrderHeaders', "$filter=LifeCycleStatus eq 'A'");
+    let rejectedSalesOrdersCount = await clientAPI.count('/HomeScreenWidgetApp/Services/com_sap_edm_sampleservice_v4.service', 'SalesOrderHeaders', "$filter=LifeCycleStatus eq 'R'");
     let platformModule = clientAPI.nativescript.platformModule;
 
     if (platformModule.isIOS) {
