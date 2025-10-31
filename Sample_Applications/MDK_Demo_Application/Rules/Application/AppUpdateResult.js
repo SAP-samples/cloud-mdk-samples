ea08900e97e0671b620cd5e517aa71917bd7fd95
Bill Froelich <50496585+billfroelich@users.noreply.github.com>
2025-10-31 00:20:46 -0500

    Consistency Changes, Use i18n for all strings

--- Sample_Applications/MDK_Demo_Application/Rules/Application/AppUpdateResult.js
+++ Sample_Applications/MDK_Demo_Application/Rules/Application/AppUpdateResult.js
@@ -9,12 +9,13 @@ export default function AppUpdateResult(context) {
     // Force a small pause to let the progress banner show in case there is no new version available
     return sleep(500).then(function () {
         let result = context.actionResults.AppUpdate.data;
+
         let versionNum = result.split(': ')[1];
         if (result.startsWith('Current version is already up to date')) {
             return context.getPageProxy().executeAction({
                 "Name": "/MDKDemoApp/Actions/GenericToastMessage.action",
                 "Properties": {
-                    "Message": `You are already using the latest version: ${versionNum}`
+                    "Message": `${context.localize('app_update_latest_version')}: ${versionNum}`
                 }
             });
         }
