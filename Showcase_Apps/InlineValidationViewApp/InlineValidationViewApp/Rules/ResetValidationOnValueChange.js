7b17866c4f8ba8aaf6de2e10ab4a5458f11e5290
Robin Kuck <kuck.robin@gmail.com>
2025-11-01 13:19:18 +0000

    Remove test message

--- Showcase_Apps/InlineValidationViewApp/InlineValidationViewApp/Rules/ResetValidationOnValueChange.js
+++ Showcase_Apps/InlineValidationViewApp/InlineValidationViewApp/Rules/ResetValidationOnValueChange.js
@@ -2,7 +2,7 @@
  * @param {IFormCellProxy} formCellProxy
  */
 export default function ResetValidationOnValueChange(formCellProxy) {
-    formCellProxy.getValidation().setMessage('test');
+    formCellProxy.getValidation().setMessage('');
     formCellProxy.getValidation().setVisible(false);
     if (!formCellProxy.nativescript.platformModule.isAndroid) {
         formCellProxy.getValidation().setSeparatorVisible(false);
