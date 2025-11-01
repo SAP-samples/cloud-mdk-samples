4c877fcc95a3fb3cde70fc8eac932eaf35d2282e
Robin Kuck <kuck.robin@gmail.com>
2025-11-01 13:19:19 +0000

    Replace binding with odata operation when pressing next and previous button

--- Showcase_Apps/SiblingNavigationApp/SiblingNavigationApp/Rules/SalesOrderHeaders_List/OnPressObjectCell.js
+++ Showcase_Apps/SiblingNavigationApp/SiblingNavigationApp/Rules/SalesOrderHeaders_List/OnPressObjectCell.js
@@ -1,10 +1,10 @@
 /**
  * @param {IClientAPI} clientAPI
  */
-export default function OnPressObjectCell(clientAPI) {
+export default async function OnPressObjectCell(clientAPI) {
     let actionBinding = clientAPI.getPageProxy().getActionBinding();
     actionBinding.Index = clientAPI.getIndex() + 1;
-    actionBinding.Total = clientAPI.getPageProxy().evaluateTargetPathForAPI('#Page:-Current/#Control:SectionedTable').getSections()[0].binding.length;
+    actionBinding.Total = await clientAPI.count('/SiblingNavigationApp/Services/com_sap_edm_sampleservice_v4.service', 'SalesOrderHeaders');
     clientAPI.getPageProxy().setActionBinding(actionBinding);
     return clientAPI.executeAction('/SiblingNavigationApp/Actions/com_sap_edm_sampleservice_v4/SalesOrderHeaders/NavToSalesOrderHeaders_Detail.action');
 }
