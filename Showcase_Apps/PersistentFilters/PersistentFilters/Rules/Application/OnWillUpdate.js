c0c5ca3117f6311f6934f72d6b88fb50b6ca8c23
Robin Kuck <kuck.robin@gmail.com>
2025-11-01 13:19:19 +0000

    Template Update

--- Showcase_Apps/PersistentFilters/PersistentFilters/Rules/Application/OnWillUpdate.js
+++ Showcase_Apps/PersistentFilters/PersistentFilters/Rules/Application/OnWillUpdate.js
@@ -5,7 +5,7 @@
 export default function OnWillUpdate(clientAPI) {
     return clientAPI.executeAction('/PersistentFilters/Actions/Application/OnWillUpdate.action').then((result) => {
         if (result.data) {
-            return clientAPI.executeAction('/PersistentFilters/Actions/Service/CloseOffline.action').then(
+            return clientAPI.executeAction('/PersistentFilters/Actions/SampleServiceV4/Service/CloseOffline.action').then(
                 (success) => Promise.resolve(success),
                 (failure) => Promise.reject('Offline Odata Close Failed ' + failure));
         } else {
