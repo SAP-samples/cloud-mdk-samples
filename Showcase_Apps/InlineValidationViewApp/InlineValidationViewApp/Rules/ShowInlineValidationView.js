5d1903aff7984a4970f65c42c250a75d257ed9e0
Robin Kuck <kuck.robin@gmail.com>
2025-11-01 13:19:18 +0000

    Inline Validation View App: Migrate app to MDK 24.11.

--- Showcase_Apps/InlineValidationViewApp/InlineValidationViewApp/Rules/ShowInlineValidationView.js
+++ Showcase_Apps/InlineValidationViewApp/InlineValidationViewApp/Rules/ShowInlineValidationView.js
@@ -5,17 +5,10 @@
  * @param {boolean} isHidden 
  */
 export default function ShowInlineValidationView(clientAPI, control, errorText, isHidden = false) {
-    control.setValidationProperty("ValidationMessage", errorText);
-    control.setValidationProperty("ValidationViewIsHidden", isHidden);
-    // no color for message text and background color required for Android
+    control.getValidation().setMessage(errorText);
+    control.getValidation().setVisible(!isHidden);
     if (!clientAPI.nativescript.platformModule.isAndroid) {
-        control.setValidationProperty("ValidationMessageColor", "5b738b");
-        control.setValidationProperty("ValidationViewBackgroundColor", "fbebf3");
-        // no separator required for Android + Web
-        if (clientAPI.nativescript.platformModule.isIOS) {
-            control.setValidationProperty("SeparatorIsHidden", isHidden);
-            control.setValidationProperty("SeparatorBackgroundColor", "d20a0a");
-        }
+        control.getValidation().setSeparatorVisible(!isHidden);
     }
-    control.applyFormCellValidation();
+    control.redraw();
 }
